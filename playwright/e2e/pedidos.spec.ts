b4c1b3e7b94039cba2508101f4779d5838cf0ca2
Lucas Siman <lucassiman@hotmail.com>
2026-01-31 16:48:05 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert
+
 test('test', async ({ page }) => {
+
+  // Arrange
   await page.goto('http://localhost:5173/');
-  //Checkpoint 1: Verificar se o título da página é "Velô Sprint"
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint 2: Verificar se a página de consulta de pedidos é carregada
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-CPEA6E');
   await page.getByTestId('search-order-button').click();
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-CPEA6E');
 
