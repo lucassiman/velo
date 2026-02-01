e3b2ce47ab42a0d90b36a4ece8b42412f0f41b2d
Lucas Siman <lucassiman@hotmail.com>
2026-02-01 10:22:22 -0300

    uso do matchAriaSnapshot alem de outros tipos de validacao

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -34,3 +34,38 @@ test('test', async ({ page }) => {
   await expect(page.getByText('APROVADO')).toBeVisible();
 
 });
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+
+  // Test Data
+  const order = 'VLO-ABC123';
+
+  // Arrange
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  //Act
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+  // Assert
+
+  // await expect(page.locator('#root')).toContainText('Pedido não encontrado');
+  // await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente');
+
+  // const title = page.getByRole('heading', { name: 'Pedido não encontrado' });
+  // await expect(title).toBeVisible();
+
+  // const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' });
+  // await expect(message).toBeVisible();
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `);
+
+});
