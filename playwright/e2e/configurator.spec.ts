fff08c801206721307a8dab4aaed78046d67e0c2
Lucas Siman <lucassiman@hotmail.com>
2026-07-28 22:19:56 -0300

    ajustes na validação da imagem do configurador de veículos

--- playwright/e2e/configurator.spec.ts
+++ playwright/e2e/configurator.spec.ts
@@ -12,7 +12,7 @@ test.describe('Configuração do Veículo', () => {
     await app.configurator.selectColor('Midnight Black')
     await app.configurator.expectTotalPrice('R$ 40.000,00')
 
-    await app.configurator.expectCarImageSrc('/src/assets/midnight-black-aero-wheels.png')
+    await app.configurator.expectCarImageSrc(/midnight-black-aero-wheels/)
   })
 
   test('alterar as rodas do veículo deve atualizar o preço de venda', async ({ app }) => {
@@ -20,11 +20,11 @@ test.describe('Configuração do Veículo', () => {
 
     await app.configurator.selectWheels(/Sport Wheels/)
     await app.configurator.expectTotalPrice('R$ 42.000,00')
-    await app.configurator.expectCarImageSrc('/src/assets/glacier-blue-sport-wheels.png')
+    await app.configurator.expectCarImageSrc(/glacier-blue-sport-wheels/)
 
     await app.configurator.selectWheels(/Aero Wheels/)
     await app.configurator.expectTotalPrice('R$ 40.000,00')
-    await app.configurator.expectCarImageSrc('/src/assets/glacier-blue-aero-wheels.png')
+    await app.configurator.expectCarImageSrc(/glacier-blue-aero-wheels/)
   })
 
   test('CT03 - opcionais atualizam o preço e o checkout persiste o total', async ({
