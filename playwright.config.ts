13546e6ca767aa031dc6f3691c666dd287158ce7
Lucas Siman <lucassiman@hotmail.com>
2026-08-11 08:12:30 -0300

    adicionando o deploy via github actions com secret correta

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://velo-lucassiman.vercel.app/',
+    //baseURL: 'https://velo-lucassiman.vercel.app/',
+    baseURL: 'https://velo-craj918z6-siman4.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
