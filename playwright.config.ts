06962b200cc696685a85e0347f74cb3a4fe43e12
Lucas Siman <lucassiman@hotmail.com>
2026-08-13 08:23:36 -0300

    estágio de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     //baseURL: 'https://velo-lucassiman.vercel.app/',
-    baseURL: 'https://velo-craj918z6-siman4.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
